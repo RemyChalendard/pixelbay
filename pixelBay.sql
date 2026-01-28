4f7ff789d16c20fcb58244cf5ac6bbf4823e8bad
RemyChalendard <chalendard.remy@gmail.com>
2026-01-28 15:53:21 +0100

    exercice11

--- pixelBay.sql
+++ pixelBay.sql
@@ -13,7 +13,7 @@ INSERT INTO utilisateur (nom, email, role, password) VALUES
 ('Chalendard Remy', 'chalendard.remy@gmail.com', 'ADMIN', '123456'),
 ('Nicolas Mallet', 'nicolat.mallet@gmail.com', 'ADMIN', 'ABCDEFG'),
 ('Melissa Tournerie', 'melissa.tournerie@gmail.com', 'CLIENT', '987654321'),
-('Julie Cassagne', 'julie.cassage@gmail.com', 'CLIENT', 'AZERTYUIOP'),
+('Julie Cassagne', 'julie.cassagne@gmail.com', 'CLIENT', 'AZERTYUIOP'),
 ('Mehdi Heizzi', 'mehdi.herizzi@gmail.com', 'CLIENT', 'nbvcxw');
 
 
@@ -33,6 +33,12 @@ INSERT INTO `commande` (`order_id`, `order_date`, `order_status`, `user_id`) VAL
 (3, '2026-01-27', 'En cours', 5),
 (4, '2026-01-26', 'Expédié', 3),
 (5, '2026-01-25', 'Expédié', 4);
+
+INSERT INTO `commande` ( `order_status`, `user_id`) VALUES
+(  'En cours', 1);
+INSERT INTO `orderproduct` (`order_id`, `produit_id`) VALUES
+(LAST_INSERT_ID(), 2);
+
 
 /*Produits*/
 CREATE TABLE produit(
@@ -91,10 +97,6 @@ FOREIGN KEY (order_id) REFERENCES (produit_id)
 )
 
 INSERT INTO `orderproduct` (`order_id`, `produit_id`) VALUES
-(1, 1),
-(1, 2),
-(2, 2),
-(1, 5),
 (3, 2),
 (4, 4),
 (1, 6),
@@ -102,22 +104,19 @@ INSERT INTO `orderproduct` (`order_id`, `produit_id`) VALUES
 (2, 5);
 
 /* Afficher le nombre de produit par catégorie */
-SELECT categorie_id, COUNT(*) AS nombre_produits
+SELECT nom, COUNT(*) AS total
 FROM produit
-GROUP BY categorie_id
-ORDER BY nombre_produits DESC;
-
+GROUP BY nom, categorie_id;
 
 /* Écrire une requête pour trouver tous les produits en stock triés par prix croissant */
-SELECT categorie_id, COUNT(*) AS nombre_produits
-FROM produit
-GROUP BY categorie_id
-ORDER BY nombre_produits ASC;
+SELECT * FROM produit
+WHERE stock = TRUE ORDER BY prix ASC;
+
 
 /*Écrire une requête pour sélectionner tous les jeux RPG en stock*/
 SELECT *
 FROM produit
-WHERE categorie_id = 1;
+WHERE categorie_id = 1 AND stock = TRUE;
 
 /*Mettre a jour un prix*/
 UPDATE produit
@@ -140,4 +139,28 @@ FROM produit
 GROUP BY categorie_id;
 
 /* Écrire une requête pour trouver le produit le plus cher et le moins cher */
+SELECT MIN(prix) AS prix_min,
+MAX(prix) AS prix_max FROM produit;
+
+/* Écrire une requête pour compter le nombre de commandes par utilisateur */
+SELECT user_id, COUNT(order_id) AS nombre_commandes
+FROM commande
+GROUP BY user_id;
+
+/* Écrire une requête qui affiche pour chaque commande :
+Le nom et email de l'utilisateur
+La date et le statut de la commande
+Les produits commandés avec leur prix */
+
+SELECT 
+c.order_date, c.order_status, u.nom, u.email, p.nom, prix
+FROM commande c 
+INNER JOIN utilisateur u ON c.user_id = u.user_id
+INNER JOIN orderproduct op ON op.order_id = c.order_id
+INNER JOIN produit p ON op.produit_id = p.produit_id;
+
+
+
+
+
 
